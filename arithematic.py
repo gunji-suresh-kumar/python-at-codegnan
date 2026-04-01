30f96dc4faebd3efaa3b369b801801972ebb7421
gunji-suresh-kumar <gunjisureshkumar97@gmail.com>
2026-04-01 14:23:05 +0530

    Add files via upload

---
+++ arithematic.py
@@ -0,0 +1,17 @@
+a=10;
+b=20;
+c=30;
+d=a+b+c;
+print('sum of all numbers',d)
+a=10;
+b=20;
+c=b-a;
+print('sub of all numbers',c)
+a=10;
+b=20;
+c=a*b;
+print('mul of all numbers',c)
+a=10;
+b=20;
+c=a/b;
+print('division of all numbers',c)
